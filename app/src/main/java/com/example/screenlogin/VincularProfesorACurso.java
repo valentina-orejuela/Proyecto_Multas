9758ebcf23dd71f25ab21747efee3a1e98f6d607
Andrés Gutiérrez <andresmauricio0127@hotmail.com>
2021-04-02 16:01:27 -0500

    Primer aporte Andrés

---
+++ app/src/main/java/com/example/screenlogin/VincularProfesorACurso.java
@@ -0,0 +1,14 @@
+package com.example.screenlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VincularProfesorACurso extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vincular_profesor_a_curso);
+    }
+}
